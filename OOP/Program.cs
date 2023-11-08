ece8b539871fdfbc833405fb70c4e2e1c11b4cfd
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-11-08 20:01:55 +0300

    classlar

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using OOP;
+
+Urun urun1 = new Urun();
+urun1.Adi = "elma";
+urun1.Fiyati = 15;
+urun1.Aciklama = "amasya elması";
+
+Urun urun2 = new Urun();
+urun2.Adi = "elma";
+urun2.Fiyati = 15;
+urun2.Aciklama = "amasya elması";
+
+Urun[] urunler = new Urun[] {urun1,urun2};
+
+foreach (Urun urun in urunler)
+{
+    Console.WriteLine(urun.Adi);
+    Console.WriteLine(urun.Fiyati);
+    Console.WriteLine(urun.Aciklama);
+    Console.WriteLine("---------------------------");
+}
+
+Console.WriteLine("-------------Metotlar-------------");
+
+//instance - örnek
+SepetManager sepetManager = new SepetManager();
+sepetManager.Ekle(urun1);
+sepetManager.Ekle(urun2);
+
---
+++ OOP/SepetManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi: "+urun.Adi);
+        }
+    }
+}
