dcd4447dd97e09315764fc570e8799dec2ae7f46
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-11-09 15:21:11 +0300

    list

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,8 @@
+﻿
+string[] isimler = new string[] {"engin","murat","halil" };
+
+List<string> isimler2 = new List<string> { "engin", "murat", "halil" };
+isimler2.Add("ilker");
+Console.WriteLine(isimler2[3]);
+Console.WriteLine(isimler2[0]);
+
