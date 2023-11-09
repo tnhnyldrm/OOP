9442e79407c4ac42e39b6785df426cc601232d40
tnhnyldrm <46500254+tnhnyldrm@users.noreply.github.com>
2023-11-09 19:28:22 +0300

    interface, method injection

---
+++ OOP3/Program.cs
@@ -0,0 +1,15 @@
+﻿using OOP3;
+
+IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+IKrediManager tasitKrediManager = new TasitKrediManager();
+IKrediManager konutKrediManager = new KonutKrediManager();
+
+ILoggerService databaseLoggerService = new DatabaseLoggerService();
+ILoggerService fileLogerService = new FileLoggerService();
+
+BasvuruManager basvuruManager=new BasvuruManager();
+basvuruManager.BasvuruYap(ihtiyacKrediManager, fileLogerService);
+
+List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager };
+//basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
